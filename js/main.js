4ca507c927fc3f7811659e4d3ad53e4fae085a01
Hien Truong <truongminhhien191@gmail.com>
2023-03-08 17:14:07 +0700

    new wedding

--- js/main.js
+++ js/main.js
@@ -274,7 +274,7 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-    let birthday = "Dec 15, 2023 00:00:00",
+    let birthday = "Apr 30, 2023 00:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {
             let now = new Date().getTime(),
